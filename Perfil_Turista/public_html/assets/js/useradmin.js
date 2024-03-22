8bc69379644f60470259724f8707370cd5717463
Dcardales <dcardales260@gmail.com>
2024-03-21 21:29:29 -0500

    inicio

---
+++ Perfil_Turista/public_html/assets/js/useradmin.js
@@ -0,0 +1,68 @@
+/*===== EXPANDER MENU  =====*/
+const showMenu = (toggleId, navbarId, bodyId) => {
+  const toggle = document.getElementById(toggleId),
+    navbar = document.getElementById(navbarId),
+    bodypadding = document.getElementById(bodyId)
+
+  if (toggle && navbar) {
+    toggle.addEventListener('click', () => {
+      navbar.classList.toggle('expander')
+
+      bodypadding.classList.toggle('body-pd')
+    })
+  }
+}
+showMenu('nav-toggle', 'navbar', 'body-pd')
+
+/*===== LINK ACTIVE  =====*/
+const linkColor = document.querySelectorAll('.nav__link')
+function colorLink() {
+  linkColor.forEach(l => l.classList.remove('active'))
+  this.classList.add('active')
+}
+linkColor.forEach(l => l.addEventListener('click', colorLink))
+
+
+/*===== COLLAPSE MENU  =====*/
+const linkCollapse = document.getElementsByClassName('collapse__link')
+var i
+
+for (i = 0; i < linkCollapse.length; i++) {
+  linkCollapse[i].addEventListener('click', function () {
+    const collapseMenu = this.nextElementSibling
+    collapseMenu.classList.toggle('showCollapse')
+
+    const rotate = collapseMenu.previousElementSibling
+    rotate.classList.toggle('rotate')
+  })
+}
+
+// Función para abrir la ventana modal
+function abrirModal() {
+  document.getElementById("modalCambiarContraseña").style.display = "block";
+}
+
+// Función para cerrar la ventana modal
+function cerrarModal() {
+  document.getElementById("modalCambiarContraseña").style.display = "none";
+}
+
+// Evento para abrir la ventana modal al hacer clic en el botón "Cambiar Contraseña"
+document.getElementById("btnCambiarContraseña").addEventListener("click", abrirModal);
+
+// Evento para cerrar la ventana modal al hacer clic en el botón de cerrar
+document.getElementsByClassName("close")[0].addEventListener("click", cerrarModal);
+
+// Función para manejar la edición de usuarios
+function editarUsuario() {
+  // Aquí puedes agregar la lógica para editar el usuario
+  // Por ejemplo, puedes abrir un formulario de edición, enviar una solicitud al servidor, etc.
+  console.log("Editar usuario"); // Esto es solo para propósitos de demostración, puedes reemplazarlo con la lógica real
+}
+
+// Event delegation para manejar clics en botones de editar
+document.addEventListener('click', function(event) {
+  if (event.target.classList.contains('btn-editar')) {
+    editarUsuario();
+  }
+});
